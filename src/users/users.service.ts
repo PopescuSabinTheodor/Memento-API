222f0ef61a539a67dc94ce2816bac167187d02d4
PopescuSabinTheodor <popescusabintheodor@gmail.com>
2023-03-05 17:59:57 +0200

    Add get user endpoint

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { NotFoundException } from 'src/error/exceptions/NotFoundException';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User, UserDocument } from './user.model';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectModel('User') private readonly userModel: Model<UserDocument>,
+  ) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
@@ -12,8 +20,12 @@ export class UsersService {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string): Promise<User> {
+    const user = await this.userModel.findById({ _id: id }).exec();
+    if (!user) {
+      throw new NotFoundException(User.name, id);
+    }
+    return user;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
